bc132e125f047c4acebb4f0347dda0fe15c843da
Debfevlo <deborahfevlo@gmail.com>
2026-05-28 22:21:31 +0000

    deleted some folders

--- healthtech/src/app/(auth)/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import { RegisterForm } from "@/features/auth/components/RegisterForm";
-
-export const metadata = {
-  title: "Create account | Healthtech",
-};
-
-export default function RegisterPage() {
-  return <RegisterForm />;
-}
