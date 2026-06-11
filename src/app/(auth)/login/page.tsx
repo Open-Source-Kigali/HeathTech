721ca1f4980e4cc7e7bc952c3edd4f08306db400
deborahfevlo <deborahfevlo@gmail.com>
2026-06-11 19:55:06 +0000

    chore: remove Next.js setup to begin React + Vite migration

--- src/app/(auth)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "@/features/auth/components/LoginForm";
-
-export const metadata = {
-  title: "Sign in | Healthtech",
-};
-
-export default function LoginPage() {
-  return <LoginForm />;
-}
--- src/features/auth/index.ts
+++
@@ -1,5 +0,0 @@
-export { LoginForm } from "./components/LoginForm";
-export { RegisterForm } from "./components/RegisterForm";
-export { authService } from "./services";
-export { loginSchema, registerSchema } from "./types";
-export type { LoginFormData, RegisterFormData } from "./types";
